377948911414cb524187f4d3d291740260b12d76
SyedaBismaa <syedabisma173@gmail.com>
2026-02-09 12:04:50 +0530

    dbsetup with neon db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './config/schema.tsx',
+  schema: './config/schema.tsx ',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
